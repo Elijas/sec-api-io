09dd1a6e7c7bdf7f259233eaf6d90f92de2e6b48
Asapanna Rakesh <rakeshark22@gmail.com>
2023-11-13 17:38:58 +0530

    feat(retry): raise exeption for 404.

--- sec_api_io/retry.py
+++ sec_api_io/retry.py
@@ -2,13 +2,13 @@ from httpx._exceptions import HTTPStatusError
 import random
 import time
 
- 
+
 def retry_with_exponential_backoff(
     func,
     initial_delay: float = 0.1,
     exponential_base: float = 2,
     jitter: bool = True,
-    max_retries: int = 10,
+    max_retries: int = 15,
     errors: tuple = (HTTPStatusError,),
 ):
 
@@ -25,6 +25,14 @@ def retry_with_exponential_backoff(
  
             # Retry on specific errors
             except errors as e:
+                if e.response.status_code==404:
+                    # Do not retry on 404 status code.
+                    # This code block is highly unlikely to be reached when URL is fetched from metadata.
+                    raise HTTPStatusError(f"404 Not Found (Client Error). The URL may not be a correct "
+                                          f"filing type or section ID my be wrong. "
+                                          f"Filing URL: {e.request.url.params._dict['url']}. "
+                                          f"Secton ID: {e.request.url.params._dict['item']}.", 
+                                          request=e.request, response=e.response,)
                 num_retries += 1
                 # Check if max retries has been reached
                 if num_retries > max_retries:
